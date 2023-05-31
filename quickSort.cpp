43ebbd0fac04e6db9f8c4fa598a2b88b2b12efe6
linzs-online <linzs.online@gmail.com>
2023-05-31 15:08:33 +0800

    second commit

--- quickSort.cpp
+++ quickSort.cpp
@@ -5,28 +5,32 @@
 
 template<typename T>
 void quickSort(typename std::vector<T>::iterator begin, typename std::vector<T>::iterator end) {
-    if (begin == end || std::next(begin) == end)
-        return;  // 容器为空或只有一个元素，无需排序
+    if (begin == end || begin + 1 == end) {
+        return;  // 容器为空或只有一个元素时，无需排序
+    }
 
-    auto pivot = *std::next(begin, std::distance(begin, end) / 2);
+    auto middle = *(begin + std::distance(begin, end) / 2);  // 将中间位置的元素作为基准元素
     auto left = begin;
-    auto right = std::prev(end);
+    auto right = end - 1;
 
-    while (left <= right) {
-        while (*left < pivot)
-            ++left;
-        while (*right > pivot)
-            --right;
+    while (left < right) {
+        while ( *left < middle) {
+            left++;  // 从左边找到第一个大于等于基准元素的元素
+        }
 
-        if (left <= right) {
-            std::iter_swap(left, right);
-            ++left;
-            --right;
+        while ( *right > middle) {
+            right--;  // 从右边找到第一个小于等于基准元素的元素
+        }
+
+        if (left < right) {
+            std::iter_swap(left, right);  // 交换找到的两个元素
+            left++;
+            right--;
         }
     }
 
-    quickSort(begin, std::make_move_iterator(right));
-    quickSort(std::make_move_iterator(left), end);
+    quickSort<T>(begin, left);    // 对左半部分递归排序
+    quickSort<T>(left, end);  // 对右半部分递归排序
 }
 
 int main() {
