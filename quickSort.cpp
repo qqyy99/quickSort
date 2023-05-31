a593f43149e2d8aef214bae834e4e98d3742eccc
linzs-online <linzs.online@gmail.com>
2023-05-31 17:04:07 +0800

    second commit

--- quickSort.cpp
+++ quickSort.cpp
@@ -13,7 +13,7 @@ void quickSort(typename std::vector<T>::iterator begin, typename std::vector<T>:
     auto left = begin;
     auto right = end - 1;
 
-    while (left < right) {
+    while (left <= right) {
         while ( *left < middle) {
             left++;  // 从左边找到第一个大于等于基准元素的元素
         }
@@ -22,14 +22,14 @@ void quickSort(typename std::vector<T>::iterator begin, typename std::vector<T>:
             right--;  // 从右边找到第一个小于等于基准元素的元素
         }
 
-        if (left < right) {
+        if (left <= right) {
             std::iter_swap(left, right);  // 交换找到的两个元素
             left++;
             right--;
         }
     }
 
-    quickSort<T>(begin, left);    // 对左半部分递归排序
+    quickSort<T>(begin, right + 1);    // 对左半部分递归排序
     quickSort<T>(left, end);  // 对右半部分递归排序
 }
 
